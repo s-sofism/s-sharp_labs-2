5a4c67102794dd3bdc754b6eec0815b39543352c
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 19:55:42 +0300

    Add files via upload

---
+++ laba6/Journal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using laba6;
+
+namespace Entities
+{
+    public class Journal
+    {
+        public Journal() { }
+
+        MyCustomCollection<EventNotify> ListEvents = new MyCustomCollection<EventNotify>();
+
+        public void RegistrationEvent(EventNotify e)
+        {
+            ListEvents.Add(e);
+        }
+
+        public void PrintInfo()
+        {
+            for (int i = 0; i < ListEvents.Count; i++)
+            {
+                Console.WriteLine("\nEvent description:");
+                Console.WriteLine(ListEvents[i].mess);
+                Console.WriteLine(ListEvents[i].name);
+            }
+        }
+    }
+}
