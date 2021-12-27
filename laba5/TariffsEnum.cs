473fdf50270e364c24a9b38b9aed7d5132a08f5f
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 19:39:34 +0300

    Add files via upload

---
+++ laba5/TariffsEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba5.Entities
+{
+    public enum TariffsEnum
+    {
+        ECONOM,
+        STANDART,
+        PREMIUM
+    }
+}
