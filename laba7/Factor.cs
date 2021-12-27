5eda1498d4a2dc4a8d631207d0e530c23d7d110b
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 20:18:28 +0300

    Add files via upload

---
+++ laba7/Factor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using laba6.Entities;
+
+namespace laba6.Helper
+{
+    public static class Factor
+    {
+        public static double GetFactor(Tariffs tariff)
+        {
+            return tariff switch
+            {
+                Tariffs.ECONOM => 1,
+                Tariffs.STANDART => 1.5,
+                Tariffs.PREMIUM => 2,
+                _ => throw new ArgumentNullException(nameof(tariff), "non-valid tariff")
+            };
+        }
+    }
+}
