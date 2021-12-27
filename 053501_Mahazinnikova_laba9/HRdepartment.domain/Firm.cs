ead36f62fddb706bbeb3d879aacb19f69f693cf6
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 22:06:51 +0300

    Add files via upload

---
+++ 053501_Mahazinnikova_laba9/HRdepartment.domain/Firm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRdepartment.domain
+{
+    [Serializable]
+    public class Firm
+    {
+        public List<HRDepartment> hrdepartment;
+        public string Name { get; set; }
+
+        public Firm() { }
+
+        public Firm(string name, List<HRDepartment> a)
+        {
+            Name = name;
+            hrdepartment = a;
+        }
+
+        public Firm(string name)
+        {
+            Name = name;
+            hrdepartment = new List<HRDepartment>();
+        }
+    }
+}
