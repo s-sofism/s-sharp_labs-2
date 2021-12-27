87cff61f53481c83af19866eff9ef9cea6f58ef3
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 21:50:39 +0300

    Add files via upload

---
+++ 053501_Mahazinnikova_laba9/HRdepartment.domain/HRDepartmnet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HRdepartment.domain
+{
+    [Serializable]
+    public class HRDepartment
+    {
+        public int id { get; set; }
+
+        public HRDepartment() { }
+
+        public HRDepartment(int id)
+        {
+            this.id = id;
+        }
+    }
+}
---
+++ 053501_Mahazinnikova_laba9/HRdepartment.domain/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace HRdepartment.domain
+{
+    public interface ISerializer
+    {
+        IEnumerable<Firm> DeSerializeByLINQ(string fileName);
+        IEnumerable<Firm> DeSerializeXML(string fileName);
+        IEnumerable<Firm> DeSerializeJSON(string fileName);
+        void SerializeByLINQ(IEnumerable<Firm> xxx, string fileName);
+        void SerializeXML(IEnumerable<Firm> xxx, string fileName);
+        void SerializeJSON(IEnumerable<Firm> xxx, string fileName);
+    }
+}
