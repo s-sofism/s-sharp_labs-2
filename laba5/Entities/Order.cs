9e84490e6d90aefb85ae9ee9bb3eb77e184dbc69
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 20:43:32 +0300

    Add files via upload

---
+++ laba5/Entities/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using laba5;
+using laba5.Entities;
+
+namespace Entities
+{
+    public class Order
+    {
+        private uint Distance { get; set; }
+        private uint Weight { get; set; }
+        private Tariffs tariff { get; set; } = new Tariffs();
+
+        public Order(uint distance, uint weight, TariffsEnum tariff)
+        {
+            Distance = distance;
+            Weight = weight;
+            this.tariff.Tariff = tariff;
+        }
+
+        public double GetCost()
+        {
+            double coaf = tariff.GetFactor();
+            /*
+            if (tariff.Tariff == TariffsEnum.ECONOM)
+            {
+                coaf = 1.0;
+            } 
+            else if (tariff.Tariff == TariffsEnum.STANDART)
+            {
+                coaf = 1.5;
+            }
+            else if (tariff.Tariff == TariffsEnum.PREMIUM)
+            {
+                coaf = 2.0;
+            }
+            */
+            return Distance * Weight * coaf;
+        }
+    }
+}
---
+++ laba5/Interfaces/ICustomCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba5
+{
+    interface ICustomCollection<T>
+    {
+        T this[int index] { get; set; }
+        void Reset();
+        void Next();
+        void Previous();
+        T Current();
+        int Count { get; }
+        void Add(T item);
+        void Remove(T item);
+        T RemoveCurrent();
+    }
+}
