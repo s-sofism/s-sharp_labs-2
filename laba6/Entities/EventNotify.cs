5655845518f50a82a39c8e0cdfdd381c8d152361
soff <79422463+s-sofism@users.noreply.github.com>
2021-12-27 20:40:02 +0300

    Add files via upload

---
+++ laba6/Entities/EventNotify.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities
+{
+    public class EventNotify
+    {
+        public string name { get; }
+        public string mess { get; }
+        public EventNotify(string _mess, string _name)
+        {
+            mess = _mess;
+            name = _name;
+        }
+    }
+}
